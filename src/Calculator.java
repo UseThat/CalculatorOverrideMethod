8f4e8857074c8df0f57f64f16f865553f6978ee1
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-04-21 12:21:10 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    double add(double a, double b){
+        return a +b;
+    }
+    double add(double a, double b, double c){
+        return add(a,b) +c;
+    }
+    double subtract(double a, double b){
+        return a - b;
+    }
+    double subtract(double a, double b, double c){
+        return subtract(a,b) - c;
+    }
+
+}
